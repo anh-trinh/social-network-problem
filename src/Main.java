2830a38549865707833ea89bccb88063193dff5c
anhtrinh <Anh.Trinh@coxautoinc.com>
2019-12-20 19:05:16 +0700

    count communities by heuristic

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,14 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         // 1. Generate a random social network, represented as a graph.
-        // create random graph with 50k user, each user has 1k friends
-        //Graph graph1 = new Graph();
-        //graph1.genRandomNetwork(50000, 1000);
+        //create random graph with 50k user, each user has 1k friends
+//        GenerateGraph generateGraph = new GenerateGraph();
+//        generateGraph.genRandomNetwork(50000, 1000);
 
         // 2. Generate a network with N communities, each has M members
         // create network with 5000 community, each has 100 users (500k users total)
-        //Graph graph2 = new Graph();
-        //graph2.genNetwork(5000, 100);
+//        GenerateGraph generateGraph_2 = new GenerateGraph();
+//        generateGraph_2.genNetwork(5000, 100);
 
         // 3. Develop an algorithm to find all of communities
 //        GenerateGraph generateGraph = new GenerateGraph();
